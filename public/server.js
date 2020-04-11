8c45e43dc1bde60ad269bd751da8dd87bf90d5ce
Leonardo morais <leonardomorais.contato@gmail.com>
2020-04-11 17:42:42 -0500

    Implementando a funcionalidade de buscar dados do bd e apresentá-los nas páginas do projeto de forma dinamica | Funcionalidade

--- public/server.js
+++ public/server.js
@@ -1,42 +1,7 @@
 const express = require("express");
-const nunjucks = require("nunjucks");
 const server = express();
 const db = require("./db");
-
-const ideas = [
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-    title: "Curso de programação",
-    category: "Estudo",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,inventore voluptate! Facilis dolor harum esse libero exercitationem eius, quod officiis quam pariatur sunt neque aliquam repellendus. Cumque quia eius sit!",
-    url: "",
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-    title: "Execícios",
-    category: "Saúde",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,inventore voluptate! Facilis dolor harum esse libero exercitationem eius, quod officiis quam pariatur sunt neque aliquam repellendus. Cumque quia eius sit!",
-    url: "",
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg ",
-    title: "Meditação",
-    category: "Meditação",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,inventore voluptate! Facilis dolor harum esse libero exercitationem eius, quod officiis quam pariatur sunt neque aliquam repellendus. Cumque quia eius sit!",
-    url: "",
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-    title: "Curso de programação",
-    category: "Estudo",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,inventore voluptate! Facilis dolor harum esse libero exercitationem eius, quod officiis quam pariatur sunt neque aliquam repellendus. Cumque quia eius sit!",
-    url: "",
-  },
-];
+const nunjucks = require("nunjucks");
 
 server.use(express.static("public"));
 
@@ -46,19 +11,31 @@ nunjucks.configure("views", {
 });
 
 server.get("/", function (req, res) {
-  const reversedIdeas = [...ideas].reverse();
-  let lastIdeas = [];
-  for (let idea of reversedIdeas) {
-    if (lastIdeas.length < 2) {
-      lastIdeas.push(idea);
-    }
-  }
+  db.all(`SELECT * FROM ideas`, function (err, rows) {
+    if (err) return console.log(err);
 
-  return res.render("index.html", { ideas: lastIdeas });
+    const reversedIdeas = [...rows].reverse();
+
+    let lastIdeas = [];
+
+    for (let idea of reversedIdeas) {
+      if (lastIdeas.length < 2) {
+        lastIdeas.push(idea);
+      }
+    }
+
+    return res.render("index.html", { ideas: lastIdeas });
+  });
 });
 
 server.get("/ideias", function (req, res) {
-  return res.render("ideias.html", { ideas });
+  db.all(`SELECT * FROM ideas`, function (err, rows) {
+    if (err) {
+      console.log(err);
+      return res.send("Erro no banco de dados!");
+    }
+    return res.render("ideias.html", { ideas: rows });
+  });
 });
 
 server.listen("3333");
