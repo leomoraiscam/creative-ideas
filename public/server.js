44ce4629586c5a4b43754cf27311b38ddd7b8a39
Leonardo morais <leonardomorais.contato@gmail.com>
2020-04-11 14:50:10 -0500

    Desabilitando o cache do nunjucks | Lib

--- public/server.js
+++ public/server.js
@@ -6,14 +6,15 @@ server.use(express.static("public"));
 
 nunjucks.configure("views", {
   express: server,
+  noCache: true,
 });
 
 server.get("/", function (req, res) {
-  return res.sendFile(__dirname + "/index.html");
+  return res.render("index.html");
 });
 
 server.get("/ideias", function (req, res) {
-  return res.sendFile(__dirname + "/ideias.html");
+  return res.render("ideias.html");
 });
 
 server.listen("3333");
