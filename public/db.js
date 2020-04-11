d44f00a929a7d91d4665b4d01b746cdbd9baba57
Leonardo morais <leonardomorais.contato@gmail.com>
2020-04-11 17:24:23 -0500

    Implementando um banco de dados na aplicação, criando um arquivo de configuração do banco e de querys, exportando o arquivo de coniguração e utilizando o banco de dados no server | Conexão com o BD

---
+++ public/db.js
@@ -0,0 +1,46 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./ws.db");
+
+db.serialize(function () {
+  db.run(`CREATE TABLE IF NOT EXISTS ideas(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image TEXT,
+    title TEXT,
+    category TEXT,
+    description TEXT,
+    link TEXT
+  )`);
+
+  const query = `INSERT INTO ideas(
+    image, 
+    title, 
+    category, 
+    description, 
+    link
+  ) VALUES(
+    ?,?,?,?,?
+  )`;
+  const values = [
+    "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    "Curso de programação",
+    "Estudo",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,inventore voluptate! Facilis dolor harum esse libero exercitationem eius, quod officiis quam pariatur sunt neque aliquam repellendus. Cumque quia eius sit!",
+    null,
+  ];
+  db.run(query, values, function (err) {
+    if (err) return console.log(err);
+    console.log(this);
+  });
+
+  db.all(`SELECT * FROM ideas`, function (err, rows) {
+    if (err) return console.log(err);
+    console.log(rows);
+  });
+
+  db.run(`DELETE FROM ideas WHERE id=?`, [1], function (err) {
+    if (err) return console.log(err);
+    console.log("Deletei", this);
+  });
+});
+
+module.exports = db;
