b1ef5aa8a846d6c6ce42dd4d5654100a038205be
Leonardo morais <leonardomorais.contato@gmail.com>
2020-04-11 15:31:26 -0500

    Trazendo os dados de forma dinamica | Automatação

--- public/server.js
+++ public/server.js
@@ -2,6 +2,41 @@ const express = require("express");
 const nunjucks = require("nunjucks");
 const server = express();
 
+const ideias = [
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    title: "Curso de programação",
+    category: "Estudo",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,inventore voluptate! Facilis dolor harum esse libero exercitationem eius, quod officiis quam pariatur sunt neque aliquam repellendus. Cumque quia eius sit!",
+    url: "",
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+    title: "Execícios",
+    category: "Saúde",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,inventore voluptate! Facilis dolor harum esse libero exercitationem eius, quod officiis quam pariatur sunt neque aliquam repellendus. Cumque quia eius sit!",
+    url: "",
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg ",
+    title: "Meditação",
+    category: "Meditação",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,inventore voluptate! Facilis dolor harum esse libero exercitationem eius, quod officiis quam pariatur sunt neque aliquam repellendus. Cumque quia eius sit!",
+    url: "",
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    title: "Curso de programação",
+    category: "Estudo",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,inventore voluptate! Facilis dolor harum esse libero exercitationem eius, quod officiis quam pariatur sunt neque aliquam repellendus. Cumque quia eius sit!",
+    url: "",
+  },
+];
+
 server.use(express.static("public"));
 
 nunjucks.configure("views", {
@@ -10,7 +45,7 @@ nunjucks.configure("views", {
 });
 
 server.get("/", function (req, res) {
-  return res.render("index.html");
+  return res.render("index.html", { ideias });
 });
 
 server.get("/ideias", function (req, res) {
